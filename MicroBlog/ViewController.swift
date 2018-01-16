abd8a177259ea58231e3026092d099879d3fd71b
S-heep <991207691@qq.com>
2018-01-16 14:58:45 +0800

    【MIcroBlog】创建tableView

--- MicroBlog/ViewController.swift
+++ MicroBlog/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initTableView()
+        view.addSubview(tableView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func initTableView() {
+        tableView = UITableView.init(frame: view.frame, style: .grouped)
+        tableView.delegate = self
+        tableView.dataSource = self
+        print("此处创建tableView")
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell.init(style: .value1, reuseIdentifier: "cell")
+        }
+        switch indexPath.row {
+        case 0:
+            cell?.textLabel?.text = "Jerry"
+            cell?.detailTextLabel?.text = "hop up"
+        case 1:
+            cell?.textLabel?.text = "Amy"
+            cell?.detailTextLabel?.text = "find her"
+        default:
+            cell?.textLabel?.text = "Mike"
+            cell?.detailTextLabel?.text = "hide well"
+        }
+        return cell!
+    }
+}
